24f330f3926fde011fa07b628de1327b64ff18ef
enrico <enrico.ballini@polimi.it>
2024-02-16 14:40:35 +0100

    .

---
+++ tests/unit/tests_paper_2/run_all_cases.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# NOTE: launch this code in the folder where it is located (there are some imports hardocded in the cases)
+
+export OMP_NUM_THREADS=4
+
+# Case 1 horizontal 
+# python3 case_1_horizontal_hu.py
+# python3 case_1_horizontal_ppu.py
+# python3 plot_flip_flop.py 1 horizontal
+# python3 plot_chops.py 1 horizontal
+# python3 plot_newton.py 1 horizontal
+python3 plot_beta.py 1 horizontal
+
+# Case 1 vertical
+# python3 case_1_vertical_hu.py
+# python3 case_1_vertical_ppu.py
+# python3 plot_flip_flop.py 1 vertical
+# python3 plot_chops.py 1 vertical
+# python3 plot_newton.py 1 vertical
+python3 plot_beta.py 1 vertical
+
+# Case 1 slanted non conforming
+# python3 case_1_slanted_hu.py
+# python3 case_1_slanted_ppu.py
+# python3 plot_flip_flop.py 1 slanted_non_conforming
+# python3 plot_chops.py 1 slanted_non_conforming
+# python3 plot_newton.py 1 slanted_non_conforming
+python3 plot_beta.py 1 slanted_non_conforming
+
+# # Case 1 slanted convergence
+# python3 case_1_slanted_hu_convergence.py
+# # python3 case_1_slanted_ppu_convergence.py # don't run the grid convergence of ppu
+# python3 plot_convergence.py 
+
+# # Case 2
+# python3 case_2_hu.py
+# python3 case_2_ppu.py
+# python3 plot_flip_flop.py 2 
+# python3 plot_chops.py 2
+# python3 plot_newton.py 2
+python3 plot_beta.py 2
+
+# # Case 3
+# python3 case_3_hu.py
+# python3 case_3_ppu.py
+# python3 plot_flip_flop.py 3
+# python3 plot_chops.py 3
+# python3 plot_newton.py 3
+# python3 plot_beta.py 3
+
+
+
+
+
+
