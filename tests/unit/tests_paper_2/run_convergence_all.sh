b82535e17fac792aab00f2098d4d0b0743436024
enrico <enrico.ballini@polimi.it>
2024-08-05 21:51:27 +0200

    add -u

--- tests/unit/tests_paper_2/run_convergence_all.sh
+++ tests/unit/tests_paper_2/run_convergence_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 case_1_slanted_hu_convergence.py
+python3 -u case_1_slanted_hu_convergence.py
 # python3 case_1_slanted_ppu_convergence.py # don't run the grid convergence of ppu
-python3 case_1_slanted_hu_convergence_non_conf.py
-python3 plot_convergence_alessio.py
+python3 -u case_1_slanted_hu_convergence_non_conf.py
+python3 -u plot_convergence_alessio.py
